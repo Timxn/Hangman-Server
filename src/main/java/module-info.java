b9a6df2637fa679a868e4d756a6af3bda264c123
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-06-25 19:12:54 +0200

    add gson libary (unused?) and fix build/run config

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module de.oose.gameservice {
     requires java.logging;
+    requires com.google.gson;
     exports de.oose.gameservice;
+    exports de.oose.gameservice.gamelogic;
+    exports de.oose.gameservice.api;
 }
