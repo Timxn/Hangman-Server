e85467cf23c989e7883378b7f2277b76ef5121bd
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-06-27 13:01:01 +0200

    add functions mady by DR timon

--- src/main/java/de/oose/gameservice/gamelogic/interfaces/GameController.java
+++ src/main/java/de/oose/gameservice/gamelogic/interfaces/GameController.java
@@ -5,6 +5,45 @@ import java.util.HashMap;
 
 public interface GameController {
     /**
+     * Adds a user to an existing game.
+     * @param username
+     * @param gameIdentifier the identifier of the game
+     */
+    public void joinGame(String username, int gameIdentifier);
+
+    /**
+     * removes a user from an existing game.
+     * @param username
+     * @param gameIdentifier the identifier of the game
+     */
+    public void leaveGame(String username, int gameIdentifier);
+
+    /**
+     * creates a new game.
+     * @param username
+     * @return 1000-9999 as identifier for the game
+     */
+    public String createGame(String username);
+
+    /**
+     * starts a new game.
+     * @return 0 = normal Player, 1 = god
+     */
+    public int startGame();
+
+    /**
+     * gives god the ability to set the word for the next round.
+     * @param word the new word
+     */
+    public void setWord(String word);
+
+    /**
+     * make a guess
+     * @param letter
+     */
+    public void guessLetter(char letter);
+
+    /**
      * Get the global scoreboard, provided with the integer mapped to the Username in a Hashmap
      * @return Hashmap<Username, Points>
      */
@@ -12,9 +51,9 @@ public interface GameController {
 
     /**
      * Get the word but with all unknown characters replaced by NULL and split by spaces so the client of the user has the minimum of the informations
-     * @return String (the word ex.: W NULL R D) (WORD)
+     * @return String (the word ex.: W NULL R D) (WORD) or null if no word exists yet (god has not yet set one)
      */
-    public String getWordWithUnknownCharactersBeingNULL();
+    public ArrayList<Character> getWord();
 
     /**
      * Get all characters that have been tried in an ArrayList<Character>
@@ -27,6 +66,12 @@ public interface GameController {
      * @return int
      */
     public int howManyTriesAreLeft();
+
+    /**
+     * gets if the word has been guessed.
+     * @return
+     */
+    public boolean getWordGuessed();
 
     /**
      * Return the Username of the User whos turn it is to guess a character
