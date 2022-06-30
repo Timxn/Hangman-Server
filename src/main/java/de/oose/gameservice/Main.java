97c848b6179b60611b56a564e6181029a0238523
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-06-30 22:47:03 +0200

    revert ze upsi dupsi from ze merge conflict

---
+++ src/main/java/de/oose/gameservice/Main.java
@@ -0,0 +1,16 @@
+package de.oose.gameservice;
+
+import de.oose.gameservice.api.ServerThread;
+import de.oose.gameservice.gamelogic.GameControllerImpl;
+
+import java.io.IOException;
+
+
+public class Main {
+    public static GameControllerImpl gameController;
+
+    public static void main(String[] args) throws IOException {
+        gameController = new GameControllerImpl();
+        new Thread(new ServerThread()).start();
+    }
+}
