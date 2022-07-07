1167aa6c6bf62abdce645915cdaeb28f7430d287
Timon <stronczek.timon@gmail.com>
2022-07-07 13:19:56 +0200

    Username is now capitalized and lower case

--- src/main/java/de/oose/gameservice/api/ClientThread.java
+++ src/main/java/de/oose/gameservice/api/ClientThread.java
@@ -49,7 +49,8 @@ public class ClientThread implements Runnable {
                     // login page
                     case "createRoom":
                     {
-                        this.username = message.getString("username").toUpperCase();
+                        this.username = message.getString("username").toLowerCase();
+                        this.username = this.username.substring(0, 1).toUpperCase() + this.username.substring(1);
                         JSONObject response = new JSONObject();
                         if (username.isBlank()) {
                             response.put("status", "Username is empty");
@@ -80,6 +81,7 @@ public class ClientThread implements Runnable {
                     case "joinRoom":
                     {
                         this.username = message.getString("username").toUpperCase();
+                        this.username = this.username.substring(0, 1).toUpperCase() + this.username.substring(1);
                         this.gameIdentifier = message.getString("gameID").toUpperCase();
                         JSONObject response = new JSONObject();
                         if (username.isBlank()) {
