62ac56072ce0a5b97c8ed2c17d8ac2397dc21335
mayerb0 <s6bomaye@uni-bonn.de>
2022-07-12 21:50:33 +0200

    Removed unused stuff

--- src/main/java/de/oose/gameservice/gamelogic/HighscoreUserImpl.java
+++ src/main/java/de/oose/gameservice/gamelogic/HighscoreUserImpl.java
@@ -3,7 +3,7 @@ package de.oose.gameservice.gamelogic;
 import java.io.Serializable;
 
 public class HighscoreUserImpl implements Serializable {
-    private String username;
+    private final String username;
     private Integer points;
 
     public HighscoreUserImpl(String username) {
@@ -15,9 +15,6 @@ public class HighscoreUserImpl implements Serializable {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public Integer getPoints() {
         return points;
