4cfd4f1bd108a4b8e6c4d61342c4cb4b000532f9
Timon <stronczek.timon@gmail.com>
2022-07-13 09:57:38 +0200

    added doc

--- src/main/java/de/oose/gameservice/gamelogic/utils/IllegalString.java
+++ src/main/java/de/oose/gameservice/gamelogic/utils/IllegalString.java
@@ -1,10 +1,21 @@
 package de.oose.gameservice.gamelogic.utils;
 
 public class IllegalString {
+
+    /**
+     * checks if the string contains illegal characters
+     * @param text the string to check
+     * @return true if the string contains illegal characters, false otherwise
+     */
     public static boolean isNotAlpha(String text) {
         return text == null || !text.chars().allMatch(Character::isLetter);
     }
 
+    /**
+     * checks if the string contains only alphabetical characters and numbers
+     * @param text the string to check
+     * @return returns true if the string contains only alphabetical characters and numbers, false otherwise
+     */
     public static boolean isAlphaNumeric(String text) {
         return text != null && text.chars().allMatch(Character::isLetterOrDigit);
     }
